b3df4524e12d0f6fe60ea06e5a1fd6a42022b59a
Denton Liu <liu.denton+github@gmail.com>
2016-05-23 04:01:53 -0400

    Packed function into a class

--- chat-analysis.py
+++ chat-analysis.py
@@ -3,39 +3,41 @@
 import json
 import traceback
 
-def parseTakeout(jsonFile):
+class MessagesObject:
+
     messages = []
-    senders = {}
-    parsed = json.load(jsonFile)
-    for conversation in parsed['conversation_state']:
-        for person in conversation['conversation_state']['conversation']['participant_data']:
-            if 'fallback_name' in person:
-                senders[person['id']['chat_id']] = person['fallback_name']
-            elif not person['id']['chat_id'] in senders:
-                senders[person['id']['chat_id']] = person['id']['chat_id']
 
-        for event in conversation['conversation_state']['event']:
-            try:
-                text = ''
-                if 'chat_message' in event and 'segment' in event['chat_message']['message_content']:
-                    for segment in event['chat_message']['message_content']['segment']:
-                        if 'text' in segment:
-                            text += segment['text']
-                        elif segment['type'] == 'LINE_BREAK':
-                            text += '\n'
-                    messages.append({
-                            'timestamp': int(event['timestamp']),
-                            'conversationId': conversation['conversation_id']['id'],
-                            'sender': event['sender_id']['chat_id'],
-                            'text': text
-                            })
-            except:
-                print(event)
-                traceback.print_exc()
-                return
+    def __init__(self, jsonFile):
+        senders = {}
+        parsed = json.load(jsonFile)
+        for conversation in parsed['conversation_state']:
+            for person in conversation['conversation_state']['conversation']['participant_data']:
+                if 'fallback_name' in person:
+                    senders[person['id']['chat_id']] = person['fallback_name']
+                elif not person['id']['chat_id'] in senders:
+                    senders[person['id']['chat_id']] = person['id']['chat_id']
 
-    for message in messages:
-        message["sender"] = senders[message["sender"]]
+            for event in conversation['conversation_state']['event']:
+                try:
+                    text = ''
+                    if 'chat_message' in event and 'segment' in event['chat_message']['message_content']:
+                        for segment in event['chat_message']['message_content']['segment']:
+                            if 'text' in segment:
+                                text += segment['text']
+                            elif segment['type'] == 'LINE_BREAK':
+                                text += '\n'
+                        self.messages.append({
+                                'timestamp': int(event['timestamp']),
+                                'conversationId': conversation['conversation_id']['id'],
+                                'sender': event['sender_id']['chat_id'],
+                                'text': text
+                                })
+                except:
+                    print(event)
+                    traceback.print_exc()
+                    return
 
-    messages.sort(key = lambda message: message["timestamp"])
-    return messages
+        for message in self.messages:
+            message["sender"] = senders[message["sender"]]
+
+        self.messages.sort(key = lambda message: message["timestamp"])
