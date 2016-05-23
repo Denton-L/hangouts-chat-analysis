d2252ca999aa0aada2cb1d844f253130c0b7a2f4
Denton Liu <liu.denton+github@gmail.com>
2016-05-23 01:39:18 -0400

    Corrected parsing errors

--- chat-analysis.py
+++ chat-analysis.py
@@ -6,14 +6,29 @@ def parseTakeout(jsonFile):
     messages = []
     parsed = json.load(jsonFile)
     for conversation in parsed["conversation_state"]:
-        senders = {i["id"]["chat_id"]: i["fallback_name"] for i in conversation["conversation_state"]["participant_data"]}
-        for event in parsed["conversation_state"]["event"]:
-            text = ""
-            for segment in event["chat_message"]["message_content"]["segment"]:
-                text += segment["text"]
-            messages.append({
-                    "timestamp": int(event["timestamp"]),
-                    "sender": senders[event["self_event_state"]["user_id"]["chat_id"]],
-                    "text": text
-                    })
+        senders = {
+                i["id"]["chat_id"]:
+                    i["fallback_name"] if "fallback_name" in i else i["id"]["chat_id"]
+                for i in conversation["conversation_state"]["conversation"]["participant_data"]
+                }
+        for event in conversation["conversation_state"]["event"]:
+            try:
+                text = ""
+                if "chat_message" in event and "segment" in event["chat_message"]["message_content"]:
+                    for segment in event["chat_message"]["message_content"]["segment"]:
+                        if "text" in segment:
+                            text += segment["text"]
+                        elif segment["type"] == "LINE_BREAK":
+                            text += "\n"
+                        else:
+                            print(segment)
+                    messages.append({
+                            "timestamp": int(event["timestamp"]),
+                            "conversationId": conversation["conversation_id"]["id"],
+                            "sender": senders[event["self_event_state"]["user_id"]["chat_id"]],
+                            "text": text
+                            })
+            except:
+                print(event)
+                return
     return messages
